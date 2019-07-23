ac2fb9f7cb29fe1267dfffe59dab51590232aeb8
Emil Tomov <bemcho@gmail.com>
2019-07-23 12:54:39 +0300

    Axes finsly drawn as wanted

--- websocket.jl
+++ websocket.jl
@@ -25,46 +25,46 @@ function processRequest()
 
         x, y, z = g = json["gyroscope"]
         #push!(gyro, g)
-        plt1 =  plotAxes(x, y, z, "Gyro")
+        plt1 =  plotAxes(x, y, z, "Gyro", 60000)
         x, y, z = g = json["accelerometer"]
        # push!(accelero, g)
         #x, y, z = [[xyz[1] for xyz in accelero],[xyz[2] for xyz in accelero],[xyz[3] for xyz in accelero]]
-        plt2 = plotAxes(x, y, z, "Accelero") 
+        plt2 = plotAxes(x, y, z, "Accelero", 2000) 
 
         x, y, z = g = json["magnetometer"]
         #push!(magneto, g)
         #x, y, z = [[xyz[1] for xyz in magneto],[xyz[2] for xyz in magneto],[xyz[3] for xyz in magneto]]
-        plt3 = plotAxes(x, y, z, "Magneto") 
+        plt3 = plotAxes(x, y, z, "Magneto", 1000) 
 
 
         t = json["environmentTemp"]
         push!(temp, t)
-        plt4 = Plots.plot(1:length(temp), temp, xlabel = "Time", ylabel = "Temperature", layout = 1, legend = false, color = [:red]) 
+        plt4 = Plots.plot(1:length(temp), temp, xlabel = "Time", ylabel = "Temperature", layout = 1, ylims = (-40, 120), legend = false, color = [:red]) 
 
         h = json["humidity"]
         push!(humidity, h)
-        plt6 = Plots.plot(1:length(humidity), humidity, xlabel = "Time", ylabel = "Humidity", layout = 1, legend = false, color = [:blue]) 
+        plt6 = Plots.plot(1:length(humidity), humidity, xlabel = "Time", ylabel = "Humidity", layout = 1, ylims = (0, 100), legend = false, color = [:blue]) 
 
         p = json["environmentPressure"]
         push!(pressure, p)
-        plt5 = Plots.plot(1:length(pressure), pressure, xlabel = "Time", ylabel = "Pressure", layout = 1, ylims = (0.0, 1000, 0.0), legend = false, color = [:black]) 
+        plt5 = Plots.plot(1:length(pressure), pressure, xlabel = "Time", ylabel = "Pressure", layout = 1, ylims = (260, 1260), legend = false, color = [:black]) 
         
 
         display(plot(plt1, plt2, plt3, plt4, plt5, plt6))
     end
 end
-function plotAxes(x, y, z, title, max = 500.0)
+function plotAxes(x, y, z, title, max = 500.0, origin = 0.0)
     mX = x / 2;
     mY = y / 2;
     mZ = z / 2;
     #x
-    plt1 = plot3d([-max,max], [-max,-max], [-max,-max], marker = 1, line = (:arrow, 4, :red), legend = false, title = title)
-        #y
-    plot3d!([-max,-max], [-max,max], [-max,-max], marker = 1, line = (:arrow,  4, :blue),   legend = false)
+    plt1 = plot3d([-max,max], [origin,origin], [origin,origin], marker = 1, line = (:arrow, 2, :red), legend = false, title = title)
         #z
-    plot3d!([-max,-max], [-max,-max], [-max,max], marker = 1, line = (:arrow, 4, :green),  legend = false)
+    plot3d!([origin,origin], [-max,max], [origin,origin], marker = 1, line = (:arrow,  2, :blue),   legend = false)
+        #y
+    plot3d!([origin,origin], [origin,origin], [-max,max], marker = 1, line = (:arrow, 2, :green),  legend = false)
          #R\_b
-    plot3d!([-max, if x < -max || x > max max else x end], [-max,if y < -max  || y > max max else  y end], [-max,if z < -max  || z > max max else  z end], marker = 1, line = (:arrow, 4, :black),  legend = false)
+   # plot3d!([-max, if x < -max || x > max max else x end], [-max,if y < -max  || y > max max else  y end], [-max,if z < -max  || z > max max else  z end], marker = 1, line = (:arrow, 2, :black),  legend = false)
 end
 function plotSensors(x, y, z, label, layout;color = [:black])
 
