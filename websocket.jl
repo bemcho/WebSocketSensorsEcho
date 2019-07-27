c8734f0f8caa7da6623226a3e52d067cdfbaf93c
Emil Tomov <bemcho@gmail.com>
2019-07-27 12:53:58 +0300

    new layout of plots,Clean ups

--- websocket.jl
+++ websocket.jl
@@ -2,10 +2,12 @@ using HTTP,JSON,Plots
 gr()
 gyro = []
 accelero = []
+acceleroGApplied = []
 magneto = []
 temp = []
 pressure = []
 humidity = []
+currentPosition = [0.0,0.0,0.0]
 jsons = Channel(1024)
 
 @async HTTP.WebSockets.listen("192.168.100.8", UInt16(2001)) do ws
@@ -28,43 +30,57 @@ function processRequest()
     x, y, z = g = json["gyroscope"]
         #push!(gyro, g)
     plt1 =  plotAxes(x, y, z, "Gyro", 6000)
-    x, y, z = g = json["accelerometer"]
+
+
+    xa, ya, za = g = json["accelerometer"]
+    global currentPosition+=[xa+x,ya+y,za+z]
+    push!(acceleroGApplied, currentPosition)
        # push!(accelero, g)
         #x, y, z = [[xyz[1] for xyz in accelero],[xyz[2] for xyz in accelero],[xyz[3] for xyz in accelero]]
-    plt2 = plotAxes(x, y, z, "Accelero", 1500) 
+    plt2 = plotAxes(xa, ya, za, "Accelero", 1500) 
 
     x, y, z = g = json["magnetometer"]
         #push!(magneto, g)
         #x, y, z = [[xyz[1] for xyz in magneto],[xyz[2] for xyz in magneto],[xyz[3] for xyz in magneto]]
     plt3 = plotAxes(x, y, z, "Magneto", 1000) 
 
+    plt7 = plotAccelero("AcceleroGyro", 1500);
 
     t = json["environmentTemp"]
     push!(temp, t)
-    plt4 = Plots.plot(1:length(temp), temp, xlabel = "Time", ylabel = "Temperature", layout = 1, ylims = (-40, 120), legend = false, color = [:red]) 
+    plt4 = Plots.plot(1:length(temp), temp, xlabel = "Time", ylabel = "Temperature", ylims = (-20.0, 120.0), label = "T: $t °C", layout = 1, color = [:red], size = (600, 150)) 
 
     h = json["humidity"]
     push!(humidity, h)
-    plt6 = Plots.plot(1:length(humidity), humidity, xlabel = "Time", ylabel = "Humidity", layout = 1, ylims = (0, 100), legend = false, color = [:blue]) 
+    plt5 = Plots.plot(1:length(humidity), humidity, ylabel = "Humidity", color = [:blue], ylims = (0.0, 100.0), label = "H: $h %", size = (600, 150)) 
 
     p = json["environmentPressure"]
     push!(pressure, p)
-    plt5 = Plots.plot(1:length(pressure), pressure, xlabel = "Time", ylabel = "Pressure", layout = 1, ylims = (260, 1260), legend = false, color = [:black]) 
-        
+    plt6 = Plots.plot(1:length(pressure), pressure, ylabel = "Pressure", color = [:black], ylims = (260.0, 1260.0), label = ["P: $p hPa"], size = (600, 150))
+    plt8 = plot(plt6, plt5, plt4, layout = (3, 1))    
 
-    display(plot(plt1, plt2, plt3, plt4, plt5, plt6))
-    #end
+    display(plot(plt1, plt2, plt3, plt7, plt8, size = (1000, 1000)))
+    
 end
+function normalize(x)
+    return sqrt(abs(x)) * sign(x)
+    
+end
+function plotAccelero(title, max = 500, origin = 0.0)
+
+    plot3d([xyz[1] + origin for xyz in acceleroGApplied], [xyz[2] + origin for xyz in acceleroGApplied], [xyz[3] + origin for xyz in acceleroGApplied], title = title, legend = false, line = (:path3d, :solid, :arrow, 2, :blue),xlims=(-500,500),ylims=(-500,500),zlims=(-500,500))
+end
+
 function plotAxes(x, y, z, title, max = 500.0, origin = 0.0)
     mX = x / 2;
     mY = y / 2;
     mZ = z / 2;
     #x
-    plt1 = plot3d([-(max + x),max + x], [-(origin + y),origin + y], [-(origin + z),origin + z], xlims = (-max, max), ylims = (-max, max), zlims = (-max, max), marker = 1, line = (:arrow, 2, :red), legend = false, title = title)
+    plt1 = plot3d([-(max + x),max + x], [-(origin + y),origin + y], [-(origin + z),origin + z], xlims = (-max, max), ylims = (-max, max), zlims = (-max, max), marker = 1, line = (:line, :solid, :arrow, 2, :red), legend = false, title = title)
         #z
-    plot3d!([-(origin + x),origin + x], [-(max + y),max + y], [-(origin + z),origin + z], marker = 1, line = (:arrow,  2, :blue),   legend = false)
+    plot3d!([-(origin + x),origin + x], [-(max + y),max + y], [-(origin + z),origin + z], marker = 1, line = (:line, :solid, :arrow,  2, :blue),   legend = false)
         #y
-    plot3d!([-(origin + x),origin + x], [-(origin + y),origin + y], [-(max + z),max + z], marker = 1, line = (:arrow, 2, :green),  legend = false)
+    plot3d!([-(origin + x),origin + x], [-(origin + y),origin + y], [-(max + z),max + z], marker = 1, line = (:line, :solid, :arrow, 2, :green),  legend = false)
          #R\_b
    # plot3d!([-max, if x < -max || x > max max else x end], [-max,if y < -max  || y > max max else  y end], [-max,if z < -max  || z > max max else  z end], marker = 1, line = (:arrow, 2, :black),  legend = false)
 end
